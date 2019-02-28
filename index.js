503e33004b84cf5281c06a4cca7b94c0b0385258
Ingo Claro <ingo@groupon.com>
2019-02-28 09:01:00 -0800

    initial version

---
+++ index.js
@@ -0,0 +1,4 @@
+const service = require('./src/backend')
+
+service.listen(3001)
+console.log('listening on port 3001')
---
+++ src/backend.js
@@ -0,0 +1,25 @@
+const Koa = require('koa')
+const route = require('koa-route')
+// const parse = require('co-body')
+
+const app = new Koa()
+
+app.use(route.get('/*', all))
+
+async function all(ctx) {
+  let headers = this.request.headers
+  let query = this.request.query
+  let env = process.env
+
+  console.log('headers', headers)
+  console.log('query', query)
+  console.log('ctx', ctx)
+
+  this.set('Access-Control-Allow-Origin', '*')
+
+  let message = env.message || 'hello'
+
+  ctx.body = { headers, query, env, message }
+}
+
+module.exports = app
